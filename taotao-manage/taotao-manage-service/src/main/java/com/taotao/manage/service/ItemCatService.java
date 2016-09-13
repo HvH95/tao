42ab843d1d17a45494f5355ed15dbd3a9da6d0c9
hjc <395276912@qq.com>
2016-09-13 09:46:11 +0800

    淘淘商城 taotao-manage

---
+++ taotao-manage/taotao-manage-service/src/main/java/com/taotao/manage/service/ItemCatService.java
@@ -0,0 +1,29 @@
+package com.taotao.manage.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.taotao.manage.mapper.ItemCatMapper;
+import com.taotao.manage.pojo.ItemCat;
+
+@Service
+public class ItemCatService extends BaseService<ItemCat> {
+
+	@Autowired
+	private ItemCatMapper itemCatMapper;
+
+	/**
+	 * 根据父节点id查询商品类目列表
+	 * 
+	 * @param parentId
+	 * @return
+	 */
+	public List<ItemCat> queryItemCatListByParentId(Long parentId) {
+		ItemCat record = new ItemCat();
+		record.setParentId(parentId);
+		return itemCatMapper.select(record);
+	}
+
+}
